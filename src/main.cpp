cb60ce95da0ed60c11ba1de803eccb7ce4652b06
Timo Fritsch <uwmub@student.kit.edu>
2023-07-14 15:56:34 +0200

    Implement command line arg reading, add stumps for tests in cmake file, add vscode settings to gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
+#include <string.h>
 
 #include "src/types.h"
 #include "rmqs/rmq.h"
 #include "rmqs/naive/naive_rmq.h"
 
 int main(int argc, char** argv){
-  std::vector<int> A = {8, 2, 5, 1, 9, 11, 10, 20, 22, 4};
-  Naive_rmq rmq(A);
-  rmq.init(A);
-  
-	std::cout << "Hello World" << std::endl;
-  std::cout << "rmq(0,9)=" << rmq.rmq(0, 9) << std::endl;
-  std::cout << "rmq(0,2)=" << rmq.rmq(0, 2) << std::endl;
-  std::cout << "rmq(4,8)=" << rmq.rmq(4, 8) << std::endl;
-	return 0;
+  if (argc != 4) {
+    std::cerr << "ERROR: Illegal number of arguments. Expected 4, but got: " << argc << std::endl;
+    return -1;
+  }
+  if (strcmp("pd",argv[1]) == 0) {
+    
+  } else if (strcmp("rmq",argv[1]) == 0) {
+    std::vector<int> A = {8, 2, 5, 1, 9, 11, 10, 20, 22, 4};
+    Naive_rmq rmq(A);
+    rmq.init(A);
+    
+    std::cout << "rmq(0,9)=" << rmq.rmq(0, 9) << std::endl;
+    std::cout << "rmq(0,2)=" << rmq.rmq(0, 2) << std::endl;
+    std::cout << "rmq(4,8)=" << rmq.rmq(4, 8) << std::endl;
+  } else {
+    std::cerr << "ERROR: Illegal data structure. Expected either 'pd' or 'rmq', but got: " << argv[1] << std::endl;
+  }
+  return 0;
 }
